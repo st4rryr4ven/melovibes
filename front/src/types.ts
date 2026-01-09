6b769d6ce6266661ac3fa9998f4fa0be7a5879de
Daniele Dainiute <daniele.dainiute@etu.umontpellier.fr>
2026-01-09 19:27:25 +0100

    feat: music front

---
+++ front/src/types.ts
@@ -0,0 +1,27 @@
+export interface User {
+  id: number;
+  login: string;
+  email: string;
+}
+
+export interface UpdateUserPayload {
+  email?: string
+  plainPassword?: string
+  currentPlainPassword: string
+}
+
+export interface LoginResult {
+  success: boolean
+  error?: string
+}
+
+export interface Music {
+  id: number;
+  title: string;
+  artists: { id: number; name: string }[];
+  genre: string[];
+  link?: string;
+  picture?: string;
+  popularity?: number;
+}
+
---
+++ front/src/views/AllMusicsPage.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import {ref} from 'vue';
+import MusicBox from '@/components/MusicBox.vue';
+import type {Music} from '@/types';
+import {apiStore} from '@/util/apiStore';
+
+const musics = ref<Music[]>([]);
+
+apiStore.getAll('musics').then(data => {
+  musics.value = data as Music[];
+});
+</script>
+
+<template>
+  <div class="musics-page">
+    <h2>Liste de toutes les musiques</h2>
+    <MusicBox v-for="music in musics" :key="music.id" :music="music"/>
+  </div>
+</template>
+
+<style scoped>
+@import "@/components/css/layout.css";
+</style>
