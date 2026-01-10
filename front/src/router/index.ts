4f62d35085d990dd5663e2ea0d177c7540f072b5
Daniele Dainiute <daniele.dainiute@etu.umontpellier.fr>
2026-01-10 15:47:00 +0100

    bugfix: front merge conflicts from develop

--- front/src/router/index.ts
+++ front/src/router/index.ts
@@ -18,13 +18,13 @@ const router = createRouter({
     {path: '/melovibes', name: 'melovibes', component: Melovibes},
     {path: '/register', name: 'register', component: Register},
     {path: '/login', name: 'login', component: Login},
-    {path: '/users', name: 'allUsers', component: AllUsers, meta:{requiresAuth: true, requiresAdmin : true}},
     {
-      path: '/profile',
-      name: 'profile',
-      component: Edit,
-      meta: {requiresAuth: true},
+      path: '/users',
+      name: 'allUsers',
+      component: AllUsers,
+      meta: {requiresAuth: true, requiresAdmin: true}
     },
+    {path: '/profile', name: 'profile', component: Edit, meta: {requiresAuth: true}},
     {path: '/music', name: 'music', component: AllMusic},
     {
       path: '/music/create',
