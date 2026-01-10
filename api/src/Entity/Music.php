a9b8859e50e203cb35c3782deb7177e84ad4c4e8
Daniele Dainiute <daniele.dainiute@etu.umontpellier.fr>
2026-01-10 18:09:23 +0100

    bugfix: artist works

--- api/src/Entity/Music.php
+++ api/src/Entity/Music.php
@@ -31,7 +31,7 @@ use Symfony\Component\Validator\Constraints as Assert;
 //            security: "(is_granted('ROLE_USER') and object == user) or is_granted('ROLE_ADMIN')"
         ),
         new GetCollection(
-            security: "is_granted('ROLE_ADMIN')"
+//            security: "is_granted('ROLE_ADMIN')"
         ),
         new GetCollection(
             uriTemplate: '/music/search',
@@ -144,11 +144,11 @@ class Music
     private ?string $link = null;
 
     #[ORM\Column]
-    #[Groups(['music:admin:read', 'serialization:music:update'])]
-    private ?bool $isValidated = null;
+    #[Groups(['music:read', 'music:admin:read', 'serialization:music:update'])]
+    private ?bool $isValidated;
 
     #[ORM\Column(nullable: true)]
-    #[Groups(['music:admin:read'])]
+    //#[Groups(['music:admin:read'])]
     private ?array $requestJSON = null;
 
     /**
@@ -226,12 +226,9 @@ class Music
         return $this;
     }
 
-    /**
-     * @return array
-     */
-    public function getArtists(): array
+    public function getArtists(): Collection
     {
-        return $this->artists->toArray();
+        return $this->artists;
     }
 
     public function addArtist(Artist $artist): static
