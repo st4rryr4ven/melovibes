6d5b3ededb38a0f152d1fe13b129bce220941423
Yanhis <yanhis.mezence@etu.umontpellier.fr>
2026-01-11 10:21:43 +0100

    fix: app.vue store => authstore

--- front/src/App.vue
+++ front/src/App.vue
@@ -15,7 +15,7 @@
         <div v-if="authStore.estConnecte && authStore.estAdmin"
              @click="router.push({ name: 'allUsers' })">Liste d'utilisateurs
         </div>
-        <div v-if="store.estConnecte && store.estAdmin()"
+        <div v-if="authStore.estConnecte && authStore.estAdmin"
              @click="router.push({ name: 'unvalidatedMusics' })">Musiques en attente de validation
         </div>
         <div v-if="authStore.estConnecte " @click="logout">Se déconnecter</div>
@@ -38,6 +38,9 @@ onMounted(() => {
   authStore.init();
 })
 const router = useRouter()
+onMounted(()=>{
+  authStore.init();
+})
 
 
 function logout() {
