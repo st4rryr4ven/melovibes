a5344dd5a4463f74806a7c32afdcfa12238480af
Daniele Dainiute <daniele.dainiute@etu.umontpellier.fr>
2026-01-17 22:08:54 +0100

    bugfix: review voter

---
+++ api/src/Security/Voter/ReviewVoter.php
@@ -0,0 +1,47 @@
+<?php
+
+// api/src/Security/Voter/ReviewVoter.php
+
+namespace App\Security\Voter;
+
+use App\Entity\Review;
+use App\Entity\User;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class ReviewVoter extends Voter
+{
+    public const EDIT = 'REVIEW_EDIT';
+    public const DELETE = 'REVIEW_DELETE';
+
+    protected function supports(string $attribute, mixed $subject): bool
+    {
+        return in_array($attribute, [self::EDIT, self::DELETE])
+            && $subject instanceof Review;
+    }
+
+    protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token): bool
+    {
+        $user = $token->getUser();
+        if (!$user instanceof UserInterface) {
+            return false;
+        }
+
+        /** @var Review $review */
+        $review = $subject;
+
+        return match($attribute) {
+            self::EDIT => $review->getAuthor() === $user,
+
+            self::DELETE => $review->getAuthor() === $user || in_array('ROLE_ADMIN', $user->getRoles()),
+
+            default => false,
+        };
+    }
+
+    private function isOwner(Review $review, User $user): bool
+    {
+        return $review->getAuthor() === $user;
+    }
+}
